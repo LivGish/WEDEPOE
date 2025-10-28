aadfdcb3968dcaca7f29a667ffd2669c4ae66b3c
LivGish <ST10486576@vcconnect.edu.za>
2025-10-28 22:30:03 +0200

    menu tabs started

--- js-assets/scripts.js
+++ js-assets/scripts.js
@@ -5,6 +5,13 @@
 /*VERSION: 1.0*/
 /*AVAILABLE: https://www.w3schools.com/howto/howto_js_accordion.asp*/
 
+/*CODE ATTRIBUTION*/
+/*TITLE: Tabs*/
+/*AUTHOR: W3 schools*/
+/*DATE: 28/10/2025*/
+/*VERSION: 1.0*/
+/*AVAILABLE: https://www.w3schools.com/howto/tabs.asp*/
+
 // Accordion
 var acc = document.getElementsByClassName("accordion-btn");
 var i;
@@ -38,4 +45,28 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
+}
+
+// Tabs
+function openMenu(evt, menuName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  document.getElementById("defaultOpen").click();
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
